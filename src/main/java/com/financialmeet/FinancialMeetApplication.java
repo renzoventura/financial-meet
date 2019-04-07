b29f608794d24f428c7bbebcc46d27d85a7b19e4
renzoventura <edrickventura1996@gmail.com>
2019-04-07 17:11:59 +1200

    Initial Commit - Basic project structure.

---
+++ src/main/java/com/financialmeet/FinancialMeetApplication.java
@@ -0,0 +1,13 @@
+package com.financialmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMeetApplication.class, args);
+	}
+
+}
